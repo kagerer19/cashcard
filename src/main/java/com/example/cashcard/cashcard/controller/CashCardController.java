48a4ccbaf699093cbd718a52b92b0e3bdc7296de
Alexander Kagerer <alexkagerer@proton.me>
2025-09-14 12:05:57 +0200

    add POST mapping

--- src/main/java/com/example/cashcard/cashcard/controller/CashCardController.java
+++ src/main/java/com/example/cashcard/cashcard/controller/CashCardController.java
@@ -3,10 +3,11 @@ package com.example.cashcard.cashcard.controller;
 import com.example.cashcard.cashcard.model.Card;
 import com.example.cashcard.cashcard.repository.CardRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -16,6 +17,27 @@ class CashCardController {
 
     CashCardController(CardRepository cardRepository) {
         this.cardRepository = cardRepository;
+    }
+
+    @PostMapping
+    private ResponseEntity<Void> createCard(@RequestBody Card newCardRequest, UriComponentsBuilder ucb){
+        Card savedCard = cardRepository.save(newCardRequest);
+        URI locationOfCard = ucb
+                .path("cashcard/{id}")
+                .buildAndExpand(savedCard.getCardId())
+                .toUri();
+        return ResponseEntity.created(locationOfCard).build();
+    }
+
+    @GetMapping("/{requestedId}")
+    private ResponseEntity<Card> findById(@PathVariable long requestedId) {
+        Optional<Card> cardOptional = cardRepository.findById(requestedId);
+
+        if(cardOptional.isPresent()){
+            return ResponseEntity.ok(cardOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/cards")
